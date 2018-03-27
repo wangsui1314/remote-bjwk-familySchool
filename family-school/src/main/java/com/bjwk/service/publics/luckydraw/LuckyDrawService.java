30abf298a3346ced05a57a4b6861b24ccefad1d3
wangsui1314 <1071680460@qq.com>
2018-03-28 05:11:10 +0800

    添加抽奖和目标计划接口

---
+++ family-school/src/main/java/com/bjwk/service/publics/luckydraw/LuckyDrawService.java
@@ -0,0 +1,11 @@
+package com.bjwk.service.publics.luckydraw;
+/** 
+* @Description: 用户抽奖实现接口
+* @author  Desolation
+* @email:1071680460@qq.com
+* @date 创建时间：2018年3月27日 下午11:50:36 
+* @version 1.0  
+*/
+public interface LuckyDrawService {
+
+}
